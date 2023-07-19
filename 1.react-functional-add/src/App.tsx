8ffda5aa0f59efd6c22694a0c2a58f615f68d1ac
v_ytaoxiong <v_ytaoxiong@wesure.cn>
2023-07-19 17:08:22 +0800

    提交demo

--- 1.react-functional-add/src/App.tsx
+++ 1.react-functional-add/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{ useState, useEffect, useRef } from 'react';
 import './App.css';
 
 /**
@@ -28,10 +28,56 @@ async function addRemote(a: number, b: number) {
  * ```
  */
 async function add(...inputs: number[]) {
+  let idArr = []
+  let childId  = []
+  if(inputs.length % 2 == 0) {
+    if(inputs.length == 0) {
+      return 0
+    }
+    for(let i = 0 ; i< inputs.length; i ++) {
+      childId.push(inputs[i])
+      if((i + 1) % 2 == 0 ) {
+        idArr.push(addRemote(childId[0],childId[1]))
+        childId = []
+      }
+    }
+    console.log("idArr",idArr)
+    let res = await Promise.all(idArr)
+    let total = res.reduce((sum, curr) => sum + curr)
+    return total
+
+  } else {
+    if(inputs.length == 1) {
+      return inputs[0]
+    }
+    for(let i = 0 ; i< inputs.length; i ++) {
+      childId.push(inputs[i])
+      if((i + 1) % 2 == 0 ) {
+        idArr.push(addRemote(childId[0],childId[1]))
+        childId = []
+      }
+    }
+    console.log("idArr",idArr)
+    let res = await Promise.all(idArr)
+    let total = res.reduce((sum, curr) => sum + curr, inputs[inputs.length - 1])
+    return total
+  }
   // 你的实现
 }
 
 function App() {
+  const [str,setStr] = useState('')
+  const [sum,setSum] = useState(0)
+  let nameRef = useRef()
+  const sumStr = () => {
+    setStr(nameRef.current.value)
+  }
+  useEffect(()=>{
+    let arr = str.split(',')
+    add(...arr).then((res)=>{
+      setSum((prevSum:any) => prevSum + res )
+    })
+  },[str])
   return (
     <div className="App">
       <header className="App-header">
@@ -39,12 +85,12 @@ function App() {
         <div>点击相加按钮能显示最终结果</div>
       </header>
       <section className="App-content">
-        <input type="text" placeholder="请输入要相加的数字（如1,3,4,5,6）" />
-        <button>相加</button>
+        <input ref={nameRef} type="text" placeholder="请输入要相加的数字（如1,3,4,5,6）" />
+        <button onClick={ sumStr }>相加</button>
       </section>
       <section className="App-result">
         <p>
-          相加结果是：<span>{'你的实现'}</span>
+          相加结果是：<span>{sum}</span>
         </p>
       </section>
     </div>
